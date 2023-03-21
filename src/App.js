650f78b0582cf2773cfa2d0ca9277fdfa3bbd109
Bhjamin <benjamin@hancock1.com>
2023-03-20 20:03:35 -0600

    bruh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./Components/Header";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
 
